5255c7f529b40f8ee12095add8aa7b6c261a9f3d
Sherwin Miller <sjm0072@mix.wvu.edu>
2020-02-27 22:18:15 -0500

    Sherwin Miller Task 07 and my project hypothesis

---
+++ Projectremodified/Code/code.r
@@ -0,0 +1,50 @@
+
+
+library(spocc)
+library(mapr)
+install.packages("rich")
+library(weatherData)
+install.packages("raster")
+library(raster)
+
+library(ggmap)
+library(devtools)
+library("devtools")
+register_google()
+
+out <- occ(query = 'Egernia richardi', from = 'gbif', limit = 10, has_coords = TRUE)
+dat <- occ2df(out)
+head(dat); tail(dat)
+
+out2 <-occ(query = "Ctenophorus maculosus", from ="gbif", limit = 10, has_coords = TRUE)
+dat2 <- occ2df(out2)
+head(dat2); tail(dat2)
+
+out3 <- occ(query = "Pogona barbata", from ="gbif", limit = 10, has_coords = TRUE)
+dat3 <- occ2df(out3)
+head(dat3); tail(dat3)
+
+out4 <- occ(query = "Tympanocryptis intima", from ="gbif", limit = 10, has_coords = TRUE)
+dat4 <- occ2df(out4)
+head(dat4); tail(dat4)
+
+out5 <- occ(query = "Diporiphora australis", from ="gbif", limit = 10, has_coords = TRUE)
+dat5 <- occ2df(out5)
+head(dat5); tail(dat5)
+
+object <- rbind(dat, dat2, dat3, dat4, dat5)
+
+write.csv(object, "Australian lizards.csv", quote = T, row.names=TRUE)
+
+setwd("C:\\Users\\Sherm\\Desktop\\Evolution\\Project\\Code")
+data <- read.csv("Australian lizards.csv")
+
+
+
+
+
+
+
+
+
+
---
+++ Projectremodified/text/Sherwin Miller hypothesis.R
@@ -0,0 +1,2 @@
+#hypothesis
+My hypothesis is that environmental conditions decrease the speciation rate in Australian lizards. 
